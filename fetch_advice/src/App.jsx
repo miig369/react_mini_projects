f3f79a4fa0987bf19d0935445d551dae127b799d
miig369 <m.gochera@outlook.com>
2023-06-09 16:35:58 +0100

    feat implement getAdvice function to use axios

--- fetch_advice/src/App.jsx
+++ fetch_advice/src/App.jsx
@@ -1,20 +1,46 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
   const [advice, setAdivce] = useState('');
 
+  // using fetch
   async function getAdvice() {
     const response = await fetch('https://api.adviceslip.com/advice');
     const data = await response.json();
+
+    /*
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+    */
 
     setAdivce(data.slip.advice);
     setCount((count) => count + 1);
   }
 
+  // using axios
+  async function getAdvice2() {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((res) => {
+        setAdivce(res.data.slip.advice);
+        setCount((count) => count + 1);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   useEffect(() => {
-    getAdvice();
+    // getAdvice();
+    getAdvice2();
   }, [0]);
 
   function handleClick() {
